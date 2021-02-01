a0e208440407de783df53cadac939513ecb8b313
Erik Zhao <erikzhao98@gmail.com>
2021-02-01 18:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your code here
 class Breakfast {
-    constuctor(food, drink) {
+    constructor(food, drink) {
         this.food = food;
         this.drink = drink;
     }
 }
 
 class Lunch {
-    constuctor(salad, soup, drink) {
+    constructor(salad, soup, drink) {
         this.salad = salad;
         this.soup = soup;
         this.drink = drink;
